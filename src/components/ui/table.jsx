35bb83a086170df9f50e063cdfc0778684d1d97a
surajyadav484 <surajryadav20@gmail.com>
2025-04-19 11:22:20 +0530

    Fix merge conflict

--- src/components/ui/table.jsx
+++ src/components/ui/table.jsx
@@ -60,7 +60,7 @@ function TableRow({
     <tr
       data-slot="table-row"
       className={cn(
-        "data-[state=selected]:bg-muted border-b transition-colors",
+        "hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
         className
       )}
       {...props} />
